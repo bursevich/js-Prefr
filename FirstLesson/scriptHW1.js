6b74aa7e439bf67895f5f1c5a887ecb630a6f580
Anastasia Bursevich <busevicha@gmail.com>
2018-06-27 21:07:05 +0300

    home work JS First ---- second commit

--- FirstLesson/scriptHW1.js
+++ FirstLesson/scriptHW1.js
@@ -1,7 +1,7 @@
 var name = prompt("Introduce yourself (in English):", "");
 name1 = name.replace(/\W/g,"");
-name1 = name1.replace(/\d/g,"");
-name1 = name1.replace("_","");
+name1 = name1.replace(/\d/g,""); 
+name1 = name1.replace(/_/g,"");    
  if (name1.length === 0 || name === "null") {
 alert("You did not enter a name");  
 } else {
