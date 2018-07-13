155575101ccec9547cdba300e4ab77c300c98dbb
Anastasia Bursevich <busevicha@gmail.com>
2018-07-13 19:34:17 +0300

    second commit for HW11 - incomplete

--- eleventhLesson/scriptHW11.js
+++ eleventhLesson/scriptHW11.js
@@ -9,6 +9,25 @@ function isInArray(arr, el){
  
  
  
+ function modifyArr(arr, el, newEl) {
+  var forEl = ["element el not found in array", arr];
+  let j = -1;
+    while ((j = arr.indexOf(el, j + 1)) != -1)  {
+    arr.splice(j,1, newEl);
+    forEl.splice(0,forEl.length, arr);
+    }  
+ return forEl;
+}
+
+cl(modifyArr([1,2,3,4,5], 3, 'hello')); // [1,2,`hello`, 4, 5]
+cl( modifyArr([1,2,3,4,5,3], 3, `hello`));  // [1,2,`hello`, 4, 5, `hello`] 
+cl(  modifyArr([1,2,3,4,5], 6, `hello`) ); // “element 6 not found in array [1,2,3,4,5]”
+ 
+
+ 
+ 
+ 
+ 
  
  
  
